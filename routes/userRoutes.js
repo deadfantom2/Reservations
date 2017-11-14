979ed579563957655b61ccdc4acb702e5fffb472
deadfantom2 <cameratest81@gmail.com>
2017-11-14 18:24:08 +0100

    possibilité modifier profile dans userRoutes et de se deconnecter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,11 @@ var User        = require('../models/user');    // import data models user
 
 
 
+//test route
+router.get('/dashboard', passport.authenticate('jwt', { session: false }), function(req, res) {
+    res.send('It worked! User id is: ' + req.user._id + '.' + req.user.email);
+});
+
 
 // Création du compte Utilisateur
 router.post('/register', function(req, res) {
@@ -66,6 +71,31 @@ router.post('/login', function(req, res) {
             });
 
         }
+    });
+});
+
+
+// log out
+router.get('/logout', function(req, res){
+    req.logOut();
+    res.status(200).send({ success: true});
+});
+
+// Utilisateur met à jour son profile
+router.put('/profile/:id', passport.authenticate('jwt', { session: false }), function(req, res) {
+    User.findByIdAndUpdate({_id: req.params.id}, req.body).then(function () {   // met à jour les idintifiants selon son id
+
+        // Rechercher utilisateur après inscription pour finir la requete
+        User.findOne({_id: req.params.id}).then(function (user) {
+            res.send(user);
+        });
+    });
+});
+
+// Supprimer utilisateur dans la DB
+router.delete('/profile/:id', passport.authenticate('jwt', { session: false }), function(req, res) {
+    User.findByIdAndRemove({_id: req.params.id}).then(function (user) {         // On passe dans la requete id d'utilisateur pour supprimmer son compte
+        res.send(user);
     });
 });
 
